4bf15f1cac2d25786dcf5e1514b0a68bfd28bca6
Menna503 <mennaallah503@gmail.com>
2024-03-08 23:45:08 +0200

    headr

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     <BrowserRouter>
        
        <Sidebar>
-       {/* <Header/> */}
+       <Header/>
      
         <Routes>
           {/* <Route  exact path="/" element={<Dashboard />} /> */}
