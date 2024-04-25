2f6f93455e812274c7abb9a9bda0ca5c210b72c0
ghada258 <ghadaelsayed20@gmail.com>
2024-04-25 17:39:44 +0200

    Logout

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,26 +1,16 @@
 
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-import Header from '../components/header';
-import { useContext ,useEffect} from 'react';
-import { UserContext } from '../UserContext';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+
 
 
 const MainLayout = ({ children }) => {
+    const token = localStorage.getItem('token');
+    if (!token)
+        return <Navigate to={'/login'} />
 
-    // const { user } = useContext(UserContext)
-    // if (!user)
-    //     return <Navigate to={'/login'} />
 
-    const navigate = useNavigate();
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            navigate('/login');
-        }
-    }, [navigate]);
 
     return (
         <>
